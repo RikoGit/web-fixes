0e2533371ddbbfaa8a1ba4a55f0d7ad1ccef3888
RikoGit <r.git@lenta.ru>
2020-07-14 22:44:37 +0300

    Add runCode

--- extension/src/content.js
+++ extension/src/content.js
@@ -1,3 +1,22 @@
-'use strict'; // eslint-disable-line strict, lines-around-directive
+'use strict';
 
+function getMessageAboutStrict() {
+    return this === undefined ? 'strict mode' : 'non-strict mode';
+}
+
+console.log(getMessageAboutStrict());
 console.log('hello from content wf!');
+
+const myCode = `
+    console.log("Hello from click");
+    console.log(location.href);
+
+`;
+
+const runCode = (code) => {
+    // eslint-disable-next-line no-new-func
+    Function(`'use strict';${code}`)();
+};
+
+// document.addEventListener('load', () => runCode(myCode));
+document.body.addEventListener('click', () => runCode(myCode));
